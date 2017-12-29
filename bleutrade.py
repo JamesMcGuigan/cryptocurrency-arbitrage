806e08f7822a031e8b633859aacd7b79fddae7a6
JamesMcGuigan <james.mcguigan.github@gmail.com>
2017-12-29 02:34:37 +0000

    Bleutrade API market class + asyncio fetch + mid_market_value

---
+++ bleutrade.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import asyncio
+from decimal import Decimal
+
+import aiohttp
+import requests
+import simplejson
+
+
+class BleuTrade:
+    urls = {
+        "base": "https://bleutrade.com/api/v2/public/",
+        "order_book": "https://bleutrade.com/api/v2/public/getorderbook?market=%s&type=ALL&depth=1000",
+        "market": "https://bleutrade.com/api/v2/public/getmarkets",
+    }
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def fetch_sync(cls, url):
+        response = requests.get(url)
+        response_json = simplejson.loads(response.text)['result']
+        return response_json
+
+    @classmethod
+    async def fetch_async(cls, url):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                text = await response.text()
+                response_json = simplejson.loads(text)['result']
+                return response_json
+
+
+class Market(BleuTrade):
+    def __init__(self, market_name, market_json, order_book_json):
+        super().__init__()
+        self.market_name = market_name
+        self.market = market_json
+        self.order_book = self.convert_order_book_to_decimal(order_book_json)
+
+    @classmethod
+    async def init_async(cls, market_name):
+        market_json = await BleuTrade.fetch_async(cls.urls['market'])
+        order_book_json = await BleuTrade.fetch_async(cls.urls['order_book'] % market_name)
+        self = Market(market_name, market_json, order_book_json)
+        return self
+
+    @classmethod
+    def convert_order_book_to_decimal(cls, order_book):
+        """
+        order_book = {
+            "buy":  [{'Quantity': '458888746.11665452',  'Rate': '0.00000001'}],
+            "sell": [[{'Quantity': '2304237552.2191453', 'Rate': '0.00000002'}],
+        }
+        :param order_book: json dict from api fetch
+        :type  order_book: dict
+        :return: order_book with all number strings converted to Decimal
+        :rtype:  dict
+        """
+        for buy_sell, order_items in order_book.items():
+            for i, order in enumerate(order_items):
+                for key, value in order.items():
+                    order_book[buy_sell][i][key] = Decimal(value)
+
+        order_book["buy"] = sorted(order_book["buy"], key=lambda d: -d['Rate'])
+        order_book["sell"] = sorted(order_book["sell"], key=lambda d: d['Rate'])
+        return order_book
+
+    def mid_market_value(self):
+        return (self.order_book["sell"][0]["Rate"] + self.order_book["buy"][0]["Rate"]) / 2
+
+
+async def main():
+    mooncoin_market = await Market.init_async("MOON_BTC")
+    print("MOON_BTC - mid_market_value: %s" % mooncoin_market.mid_market_value())
+    pass
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
